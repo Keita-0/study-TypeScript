162d5ed810fffa8146d897eb10efeea5add3f133
Keita-0 <laughlaughkaeru@gmail.com>
2021-11-30 00:37:24 +0900

    Wideningの学習

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,19 @@
 import type { NextPage } from "next";
 
-const foo: number = 10;
-const foo2: string = "test";
+const foo = "foo" as const;
+let foo2 = foo;
+
+console.log(foo2);
+
+const Components = (props: { foo: boolean }) => {
+  if (props.foo) {
+    return <div> a</div>;
+  }
+  return <div>test</div>;
+};
 
 const Home: NextPage = () => {
-  return <div>test</div>;
+  return <Components foo={true} />;
 };
 
 export default Home;
