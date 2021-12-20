df4171f42370a4e18c49d0a853cbfb96517750f4
Keita-0 <laughlaughkaeru@gmail.com>
2021-12-20 23:55:37 +0900

    2021/12/20

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
             "https://doodleipsum.com/200x200/avatar-5?bg=6392D9&i=0f33dd99912f70859db2eeaf52dd71f4",
         }}
         body={{
-          text: "おはようございます！！！",
+          text: "おはようございます！",
           image:
             "https://doodleipsum.com/700x394/flat?bg=7463D9&i=c2a3b28fed2bb5d5690e9bab3ff2dce1",
         }}
