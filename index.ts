19123191c2d9f79a7f102edc0714d3daa72bdc61
github keita-0 <laughlaughkaeru@gmail.com>
2022-05-26 23:57:31 +0900

    学習

--- index.ts
+++ index.ts
@@ -3,3 +3,21 @@ export const color = "red";
 const theme = {
   color: "red",
 } as const;
+
+type Foo = {
+  a: number;
+  b: string;
+};
+
+type bar = {
+  a: string;
+  c: boolean;
+};
+//intersection types
+type FooBar = Foo | bar;
+
+const test: FooBar = {
+  a: 2,
+  b: "test",
+  c: false,
+};
